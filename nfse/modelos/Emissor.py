f131eef1b246eae2e6cf58bc819a8c4234c0bb2e
Marcelo Belli <marcelo@belli.me>
2016-11-27 19:21:46 -0200

    Criação Modelo Emissor com os atributos necessários para consultar uma nota no webservice da prefeitura de Curitiba

---
+++ nfse/modelos/Emissor.py
@@ -0,0 +1,8 @@
+class Emissor(object):
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    cnpj = str()
+    inscricao_municipal = str()
