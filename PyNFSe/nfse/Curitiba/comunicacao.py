a64221d77135ad017d4bf85875f44b28f2d4441b
Marcelo Belli <marcelo@belli.me>
2016-11-27 22:43:13 -0200

    - Comunicação: Ajuste para selecionar ambiente piloto

--- PyNFSe/nfse/Curitiba/comunicacao.py
+++ PyNFSe/nfse/Curitiba/comunicacao.py
@@ -1,4 +1,6 @@
 from zeep import Client
+from zeep.transports import Transport
+
 
 class Comunicacao():
     _url_homologacao = 'https://pilotoisscuritiba.curitiba.pr.gov.br/nfse_ws/NfseWs.asmx?WSDL'
@@ -8,7 +10,8 @@ class Comunicacao():
 
     def __init__(self, homologacao=False):
         self._url_ambiente = self._url_homologacao if homologacao else self._url_producao
-        self._cliente = Client(self._url_ambiente)
+        verify = False if homologacao else True
+        self._cliente = Client(self._url_ambiente, transport=Transport(verify=verify))
 
     def validar_xml(self, xml):
         return self._cliente.service.ValidarXml(xml)
