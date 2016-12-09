31e70375486812000c02b8d0d3455351d0704acc
Marcelo Belli <marcelo@belli.me>
2016-12-09 13:51:18 -0200

    - Ajustes serialização: Deixa de ser uma classe para ter somente funções.

--- PyNFSe/nfse/Curitiba/serializacao.py
+++ PyNFSe/nfse/Curitiba/serializacao.py
@@ -1,33 +1,29 @@
-#from importlib import import_module
 from PyNFSe.nfse.Curitiba import schema as nfse_schema
 
-class Serializacao(object):
 
-    #def __init__(self):
-        #global nfse_schema
-        #nfse_schema = import_module('PyNFSe.nfse.Curitiba.schema')
+def consulta_nfse(prestador, numero_nfse):
 
-    def consulta_nfse(self, prestador, numero_nfse):
-        id_prestador = nfse_schema.tcIdentificacaoPrestador()
-        id_prestador.Cnpj = prestador.cnpj
-        id_prestador.InscricaoMunicipal = prestador.inscricao_municipal
+    consulta = nfse_schema.ConsultarNfseEnvio()
+    consulta.Prestador = id_prestador(prestador)
 
-        consulta = nfse_schema.ConsultarNfseEnvio()
-        consulta.Prestador = id_prestador
+    consulta.NumeroNfse = numero_nfse
 
-        consulta.NumeroNfse = numero_nfse
+    xml = consulta.toxml(element_name='ConsultarNfseEnvio')
+    xml = limpeza_xml(xml)
 
-        xml = consulta.toxml(element_name='ConsultarNfseEnvio')
-        xml = self.limpeza_xml(xml)
-
-        return xml
+    return xml
 
 
+def id_prestador(prestador):
+
+    id_prestador = nfse_schema.tcIdentificacaoPrestador()
+    id_prestador.Cnpj = prestador.cnpj
+    id_prestador.InscricaoMunicipal = prestador.inscricao_municipal
+
+    return id_prestador
 
 
+def limpeza_xml(xml):
 
-
-    def limpeza_xml(self, xml):
-
-        return xml.replace('ns1:', '').replace(':ns1', '').replace('<?xml version="1.0" ?>',
-                                                                   '<?xml version="1.0" encoding="UTF-8"?>')
+    return xml.replace('ns1:', '').replace(':ns1', '').replace('<?xml version="1.0" ?>',
+                                                               '<?xml version="1.0" encoding="UTF-8"?>')
