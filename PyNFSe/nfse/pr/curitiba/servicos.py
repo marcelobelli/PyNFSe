2f9863a243d93e21fc27f40fc78eccf1a70015a2
Marcelo Belli <marcelo@belli.me>
2016-12-18 16:45:55 -0200

    - Testes Assinatura

--- PyNFSe/nfse/pr/curitiba/servicos.py
+++ PyNFSe/nfse/pr/curitiba/servicos.py
@@ -1,6 +1,11 @@
 from PyNFSe.nfse.pr.curitiba import serializacao
+from PyNFSe.nfse.pr.curitiba import comunicacao
+
 
 class Servicos:
+
+    # def __init__(self):
+
 
     def oi(self):
         print('oi')
---
+++ PyNFSe/tests/pr/curitiba/test_curitiba_assinatura.py
@@ -0,0 +1,30 @@
+import unittest
+
+import PyNFSe.nfse.pr.curitiba.serializacao as s
+from PyNFSe.tests.pr.curitiba.basetestesserializacao import BaseTestesSerializacao, xml_expected
+from PyNFSe._utils.assinatura import Assinatura
+
+
+class AssinaturaTestCase(BaseTestesSerializacao):
+
+    def setUp(self):
+        super(AssinaturaTestCase, self).setUp()
+        namespace = '{http://isscuritiba.curitiba.pr.gov.br/iss/nfse.xsd}'
+        self.assinador = Assinatura('PyNFSe/tests/pr/curitiba/certificados/certificado.pfx', '123456', namespace)
+
+    def test_assinatura_lote_rps(self):
+
+        xml_lote_rps = s.envio_lote_rps(self.lote_rps)
+        xml_lote_rps = self.assinador.assinar_lote_rps(xml_lote_rps)
+
+        xml_lote_rps_expected = xml_expected('EnviarLoteRpsEnvio-Assinado.xml')
+
+        self.assertEqual(xml_lote_rps, xml_lote_rps_expected)
+
+    def test_assinatura_cancelamento_nfse(self):
+        xml_cancelar_nfse = s.cancela_nfse(self.pedido_cancelamento_nfse)
+        xml_cancelar_nfse = self.assinador.assinar_cancelamento_nfse(xml_cancelar_nfse)
+
+        xml_cancelar_nfse_expected = xml_expected('CancelarNfseEnvio-Assinado.xml')
+
+        self.assertEqual(xml_cancelar_nfse, xml_cancelar_nfse_expected)
