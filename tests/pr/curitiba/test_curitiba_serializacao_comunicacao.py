d031275f62bf43fec516c0ede486057de66a549a
Marcelo Belli <marcelo@belli.me>
2016-12-19 19:04:31 -0200

    - Correção: Alterado localização pastas tests

---
+++ tests/pr/curitiba/test_curitiba_serializacao_comunicacao.py
@@ -0,0 +1,66 @@
+import unittest
+import datetime
+
+import PyNFSe.nfse.pr.curitiba.serializacao as s
+from tests.pr.curitiba.basetestesserializacao import BaseTestesSerializacao, xml_expected
+
+
+class SerializacaoComunicacaoTestCase(BaseTestesSerializacao):
+
+    def test_consultar_nfse_por_nota(self):
+        numero_nota = 179
+        xml_consultar_nfse = s.consulta_nfse_por_numero(self.prestador, numero_nota)
+
+        xml_consultar_nfse_expected = xml_expected('ConsultarNfseEnvio-por_nota.xml')
+
+        self.assertEqual(xml_consultar_nfse, xml_consultar_nfse_expected)
+
+    def test_consultar_nfse_por_data(self):
+        data_inicial = datetime.datetime(2016, 12, 12)
+        data_final = datetime.datetime(2016, 12, 17)
+        xml_consultar_nfse = s.consulta_nfse_por_data(self.prestador, data_inicial, data_final)
+
+        xml_consultar_nfse_expected = xml_expected('ConsultarNfseEnvio-por_data.xml')
+
+        self.assertEqual(xml_consultar_nfse, xml_consultar_nfse_expected)
+
+    def test_envio_lote_rps(self):
+        xml_lote_rps = s.envio_lote_rps(self.lote_rps)
+
+        xml_lote_rps_expected = xml_expected('EnviarLoteRpsEnvio.xml')
+
+        self.assertEqual(xml_lote_rps, xml_lote_rps_expected)
+
+    def test_consultar_situacao_lote_rps(self):
+        xml_consultar_situacao_lote = s.consulta_situacao_lote_rps(self.prestador, '636174090357960929')
+
+        xml_consultar_situacao_lote_expected = xml_expected('ConsultarSituacaoLoteRpsEnvio.xml')
+
+        self.assertEqual(xml_consultar_situacao_lote, xml_consultar_situacao_lote_expected)
+
+    def test_consultar_nfse_por_rps(self):
+
+        xml_consultar_nfse = s.consulta_nfse_por_rps(self.rps)
+
+        xml_consultar_nfse_expected = xml_expected('ConsultarNfseRpsEnvio.xml')
+
+        self.assertEqual(xml_consultar_nfse, xml_consultar_nfse_expected)
+
+    def test_consultar_lote_rps(self):
+        xml_consultar_lote = s.consulta_lote_rps(self.prestador, '636174090357960929')
+
+        xml_consultar_lote_expected = xml_expected('ConsultarLoteRpsEnvio.xml')
+
+        self.assertEqual(xml_consultar_lote, xml_consultar_lote_expected)
+
+    def test_cancelar_nfse(self):
+        xml_cancelar_nfse = s.cancela_nfse(self.pedido_cancelamento_nfse)
+
+        xml_cancelar_nfse_expected = xml_expected('CancelarNfseEnvio.xml')
+
+        self.assertEqual(xml_cancelar_nfse, xml_cancelar_nfse_expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ tests/pr/curitiba/test_curitiba_serializacao_entidades.py
@@ -0,0 +1,46 @@
+import unittest
+
+import PyNFSe.nfse.pr.curitiba.serializacao as s
+from tests.pr.curitiba.basetestesserializacao import BaseTestesSerializacao, xml_expected
+
+
+class SerializacaoEntidadesTestCase(BaseTestesSerializacao):
+
+    def test_xml_prestador(self):
+        xml_prestador = s._serial_prestador(self.prestador)
+        self.tc_inf_rps.Prestador = xml_prestador
+        xml_prestador = xml_prestador.toxml()
+
+        xml_prestador_expected = xml_expected('prestador.xml')
+
+        self.assertEqual(xml_prestador, xml_prestador_expected)
+
+    def test_xml_tomador(self):
+        xml_tomador = s._serial_tomador(self.tomador)
+        self.tc_inf_rps.Tomador = xml_tomador
+        xml_tomador = xml_tomador.toxml()
+
+        xml_tomador_expected = xml_expected('tomador.xml')
+
+        self.assertEqual(xml_tomador, xml_tomador_expected)
+
+    def test_xml_servico(self):
+        xml_servico = s._serial_servico(self.servico)
+        self.tc_inf_rps.Servico = xml_servico
+        xml_servico = xml_servico.toxml()
+
+        xml_servico_expected = xml_expected('servico.xml')
+
+        self.assertEqual(xml_servico, xml_servico_expected)
+
+    def test_xml_rps(self):
+        xml_rps = s._serial_rps(self.rps)
+        self.tc_lote_rps.ListaRps.append(xml_rps)
+        xml_rps = xml_rps.toxml()
+
+        xml_rps_expected = xml_expected('rps.xml')
+
+        self.assertEqual(xml_rps, xml_rps_expected)
+
+if __name__ == '__main__':
+    unittest.main()
